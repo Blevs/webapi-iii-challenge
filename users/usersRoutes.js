673cbf0570b6cbaaf16c82448f8e5517f392cb0e
Henry Blevins <heblevi@gmail.com>
2019-04-24 15:54:51 -0400

    Implement get user by id and get their posts

--- users/usersRoutes.js
+++ users/usersRoutes.js
@@ -24,7 +24,29 @@ router.post('/', (req, res) => {
   }
 });
 
+// get user by id
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  userDb.getById(id)
+    .then(user => user
+          ? res.status(200).json(user)
+          : res.status(404).json({error: "The user with the specified ID does not exist."}))
+    .catch(err => res.status(404).json({error: "There was an error retrieving the user information."}));
+});
+
 // get user by id posts
+router.get('/:id/posts', (req, res) => {
+  const id = req.params.id;
+  userDb.getById(id)
+    .then(user => user
+          ? userDb.getUserPosts(id)
+          .then(posts => res.status(200).json(posts))
+          .catch(err => res.status(500).json({
+            error: "There was an error retrieving the user's posts."
+          }))
+          : res.status(404).json({error: "The user with the specified ID does not exist."}))
+    .catch(err => res.status(404).json({error: "There was an error retrieving the user information."}));
+});
 // post user by id post
 // delete user by id post by id
 // put user by id post by id
