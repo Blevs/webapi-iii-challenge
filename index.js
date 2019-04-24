4bed3e1aa1ce5a0ecc744ba5004903b760b3a7c4
Henry Blevins <heblevi@gmail.com>
2019-04-24 14:53:58 -0400

    Initialize server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code away!
+const express = require('express');
+const postsRoutes = require('./posts/postsRoutes.js');
+
+const server = express();
+server.use(express.json());
+
+server.use('/api/posts', postsRoutes);
+
+server.listen(4000, () => console.log('API running on port 4000'));
---
+++ posts/postsRoutes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const postDb = require('../data/helpers/postDb.js');
+
+const router = express.Router();
+
+module.exports = router;
