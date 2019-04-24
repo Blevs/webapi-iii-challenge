cd4ecc524da52722b32be5d79dfb0876d7c0778c
Henry Blevins <heblevi@gmail.com>
2019-04-24 14:57:37 -0400

    Implement get posts

--- posts/postsRoutes.js
+++ posts/postsRoutes.js
@@ -3,4 +3,18 @@ const postDb = require('../data/helpers/postDb.js');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  postDb.get()
+    .then(posts => res.status(200).json(posts))
+    .catch(err => res.staus(500).json({error: "The posts information could not be retrieved."}));
+});
+
+
+// post post
+
+// get post by id
+// put post by id
+// delete post by id
+
+
 module.exports = router;
